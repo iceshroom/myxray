75dcd09087646024137691e28c7f210237725f00
iceshroom <yyy118558912@gmail.com>
2026-06-27 23:05:49 +0800

    bug fix

--- auto.sh
+++ auto.sh
@@ -2,4 +2,5 @@
 apt update -y && apt upgrade -y
 apt install -y curl wget unzip python3 python3-pip git jq
 git clone --depth=1 https://github.com/iceshroom/myxray /root/myxray
-bash /root/myxray/deploy.sh
+chmod u+x /root/myxray/deploy.sh
+sh /root/myxray/deploy.sh
